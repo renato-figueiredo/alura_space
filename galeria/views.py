5e79c2fdf7857d2b002424d0bb5d42c23e4648bd
Renato Figueiredo <renatofigueiredoalves@gmail.com>
2024-03-24 18:16:52 -0300

    ADD função para barra de busca

--- galeria/views.py
+++ galeria/views.py
@@ -26,3 +26,6 @@ def imagem(request, foto_id):
     """
     fotografia = get_object_or_404(Fotografia, pk=foto_id)
     return render(request, '../templates/galeria/imagem.html', {"fotografia": fotografia})
+
+def buscar(request):
+    return render(request, '../templates/galeria/buscar.html')
