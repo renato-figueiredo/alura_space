593032f158598bb175f739895f2887e3d2ea0496
Renato Figueiredo <renatofigueiredoalves@gmail.com>
2024-03-24 12:35:34 -0300

    update admin com uma lista de categorias

--- galeria/admin.py
+++ galeria/admin.py
@@ -5,5 +5,6 @@ class ListandoFotografias(admin.ModelAdmin):
     list_display = ('id', 'nome', 'legenda')
     list_display_links = ('id', 'nome')
     search_fields = ('nome',)
+    list_filter = ('categoria',)
 
 admin.site.register(Fotografia, ListandoFotografias)
