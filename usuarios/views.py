93fac51b06abd2ac158e5c04ce9e0a5cb74a735d
Renato Figueiredo <renatofigueiredoalves@gmail.com>
2024-03-25 20:31:10 -0300

    Atualização das paginas para uso do logout

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,11 +1,23 @@
 from django.shortcuts import render, redirect
+
 from django.contrib.auth.models import User
 from django.contrib import auth
+from django.contrib import messages
 
 from usuarios.forms import LoginForms, CadastroForms
 
 
 def login(request):
+    """
+    Renderiza a página de login e lida com a funcionalidade de login.
+
+    Parâmetros:
+        request (HttpRequest): O objeto de requisição HTTP.
+
+    Retorna:
+        HttpResponse: O objeto de resposta HTTP.
+
+    """
     form = LoginForms()
 
     if request.method == 'POST':
@@ -22,8 +34,10 @@ def login(request):
         )
         if usuario is not None:
             auth.login(request, usuario)
+            messages.success(request, f'Login {nome} realizado com sucesso!')
             return redirect('index')
         else:
+            messages.error(request, 'Login ou senha inválidos!')
             return redirect('login')
 
     return render(request, 'usuarios/login.html', {'form': form})
@@ -39,6 +53,7 @@ def cadastro(request):
         
         if form.is_valid():
             if form['senha_1'].value() != form['senha_2'].value():
+                messages.error(request, 'As senhas precisam ser iguais!')
                 return redirect('cadastro')
             
             nome=form['nome_cadastro'].value()
@@ -46,6 +61,7 @@ def cadastro(request):
             senha=form['senha_1'].value()
 
             if User.objects.filter(username=nome).exists():
+                messages.error(request, 'Usuário já existe!')
                 return redirect('cadastro')
             
             usuario = User.objects.create_user(
@@ -54,6 +70,21 @@ def cadastro(request):
                 password=senha
             )
             usuario.save()
+            messages.success(request, 'Cadastrado com sucesso!')
             return redirect('login')
 
     return render(request, 'usuarios/cadastro.html', {'form': form})
+
+def logout(request):
+    """
+    Efetua o logout do usuário chamando a função `logout` do módulo `auth`.
+
+    Parâmetros:
+        request (HttpRequest): O objeto de requisição HTTP representando a requisição atual.
+
+    Retorna:
+        HttpResponseRedirect: Uma resposta de redirecionamento para a URL 'index'.
+    """
+    auth.logout(request)
+    messages.success(request, 'Logout efetuado com sucesso!')
+    return redirect('login')
