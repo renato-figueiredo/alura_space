6f410874305f365ca32cefda4700ffecb046e353
Renato Figueiredo <renatofigueiredoalves@gmail.com>
2024-03-27 10:08:17 -0300

    add delete_image

--- apps/galeria/views.py
+++ apps/galeria/views.py
@@ -86,5 +86,8 @@ def editar_imagem(request, foto_id):
 
     return render(request, 'galeria/editar_imagem.html', {'form': form, 'foto_id': foto_id})
 
-def deletar_imagem(request):
-    pass
+def deletar_imagem(request, foto_id):
+    fotografia = Fotografia.objects.get(id=foto_id)
+    fotografia.delete()
+    messages.success(request, 'Fotografia excluída com sucesso!')
+    return redirect('index')
